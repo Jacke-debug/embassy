54b6e75dc066ad15335b9964c45e58392c7d6a75
Jakob <jakob.laurell@wattstep.com>
2025-05-21 16:33:48 +0200

    fix spelling

--- embassy-stm32/src/adc/g4.rs
+++ embassy-stm32/src/adc/g4.rs
@@ -476,7 +476,7 @@ impl<'d, T: Instance> Adc<'d, T> {
             reg.set_discen(false);
             reg.set_cont(false); // False for interrupt triggered measurements
             reg.set_dmacfg(Dmacfg::ONE_SHOT);
-            reg.set_dmaen(Dmaen::DISABLED);
+            reg.set_dmaen(Dmaen::DISABLE);
         });
     }
 
